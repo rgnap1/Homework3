a08a2ffbec40e274452ba1e63f17261f9684cc9b
rgnap <osovskaya_1993@mail.ru>
2022-11-09 21:29:33 +0300

    Сommit 2. Save task1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+        //task1
+        int redApple = 208963;
+        System.out.println (" Значение переменной " + redApple + " с типом int равно " + 208963 );
+        byte greenApple = 6;
+        System.out.println(" Значение переменной " + greenApple + " с типом byte равно " + 6);
+        short orange = 31560;
+        System.out.println(" Значение переменной " + orange + " с типом short равно " + 31560);
+        long grape = 876000024;
+        System.out.println("Значение переменной " + grape + " с типом long равно " + 876000024);
+
+
+
+
+
+
+
+
+
     }
 }
