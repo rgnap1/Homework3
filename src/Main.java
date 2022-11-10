84b66c3b89f2322784a21910cf359744a605f14d
rgnap <osovskaya_1993@mail.ru>
2022-11-10 22:18:13 +0300

    Сommit 3. Save task2.

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,42 @@ public class Main {
         System.out.println(" Значение переменной " + greenApple + " с типом byte равно " + 6);
         short orange = 31560;
         System.out.println(" Значение переменной " + orange + " с типом short равно " + 31560);
-        long grape = 876000024;
-        System.out.println("Значение переменной " + grape + " с типом long равно " + 876000024);
+        long grape = 876000024L;
+        System.out.println("Значение переменной " + grape + " с типом long равно " + 876000024L);
+        float peachThree = 1.6f;
+        System.out.println("Значение переменной " + peachThree + " с типом float равно " + 1.6f);
+
+        //task2
+        double theMilesInRoad = 27.12;
+        System.out.println (theMilesInRoad);
+        long atoms = 987678965549L;
+        System.out.println (atoms);
+        float applePie = 2.786f;
+        System.out.println(applePie);
+
+        int mounths = 6;
+        System.out.println(mounths);
+        boolean theFalse = 6 < 5;
+        System.out.println(theFalse);
+
+        short books = 569;
+        System.out.println(books);
+        short snowflakes = -159;
+        System.out.println(snowflakes);
+        short bananas = 29897;
+        System.out.println(bananas);
+
+        int theLoaf = 67;
+        System.out.println(theLoaf);
+
+
+
+
+
+
+
+
+
 
 
 
